8e25e803e03a8d58e63ef9d104ef5deeac21a022
teeveeJS <teemu.eemeli99@gmail.com>
2017-12-17 15:16:20 -0800

    xlsxwriter

--- main.py
+++ main.py
@@ -6,27 +6,24 @@ class Player:
     def __init__(self, n, r, fa=False, is_f=False):
         self.name = n
         self.rating = int(r)
+        
         #these should be type-casted to bools
         self.is_free_agent = fa
         self.is_female = is_f
 
     @property
     def get_rating(self):
-        if self.rating > 2700:
-            return 2700
-        elif self.rating < 2000:
-            return 2000
-        else:
-            return self.rating
+        rtg = self.rating - int(self.is_female) * 100
+        return max(2000, min(2700, rtg))
 
 def free_agent_count(lst):
-    return sum(list(map(lambda p: p.is_free_agent, lst)))
+    return [p.is_free_agent for p in lst].count(True)
 
 def get_average_rating(lst):
     return sum(list(map(lambda p: p.get_rating, lst))) / len(lst)
 
 def has_female(lst):
-    return bool(sum(list(map(lambda p: p.is_female, lst))))
+    return any([p.is_female for p in lst])
 
 def output_lst(lst):
     out = ""
@@ -89,12 +86,7 @@ def main():
     for c in combs:
         avg = get_average_rating(c)
 
-        #bonus to the maximum average if the team contains a female player
-        add = 0
-        if has_female(c):
-            add = 10
-
-        if min_rating <= avg <= max_rating + add and free_agent_count(c) < 2:
+        if min_rating <= avg <= max_rating and free_agent_count(c) < 2:
             comb_count += 1
             valid_combs.append([c, avg])
 
@@ -107,5 +99,54 @@ def main():
         print(output_lst(valid_combs[i][0]), valid_combs[i][1])
 
 
+    #TODO: 3 OPTIONS: Display best, display all, write to excel
+
+    if input("Write to Excel (y/n)?\n>> ").strip() == "y":
+        import xlsxwriter as xw
+
+        file_name = input("Please enter file name.\n>> ").strip()
+        #TODO: validate the file name
+
+        wb = xw.Workbook(file_name + ".xlsx")
+        ws1 = wb.add_worksheet("Combinations")
+        ws2 = wb.add_worksheet("Players")
+
+        for i in range(4):
+            ws1.write(0, i, "Board {0}".format(i+1))
+        ws1.write(0, 4, "Avg Rating")
+
+        row1 = 1
+        col1 = 0
+        for c, a in valid_combs:
+            for p in c:
+                ws1.write(row1, col1, "{0} ({1})".format(p.name, p.rating))
+                col1 += 1
+            ws1.write(row1, col1, a)
+            col1 = 0
+            row1 += 1
+
+        ws2.write(0, 0, "Player Name")
+        ws2.write(0, 1, "Rating")
+        ws2.write(0, 2, "Status")
+        ws2.write(0, 3, "Gender")
+
+        row2 = 1
+        col2 = 0
+        for p in players:
+            ws2.write(row2, col2, p.name)
+            #TODO: fit column width
+            col2 += 1
+            ws2.write(row2, col2, p.rating)
+            col2 += 1
+            ws2.write(row2, col2, p.is_free_agent)
+            col2 += 1
+            ws2.write(row2, col2, p.is_female)
+            col2 = 0
+            row2 += 1
+
+        wb.close()
+
+
 if __name__ == "__main__":
     main()
+    print("May the best team (SJH) win!")
